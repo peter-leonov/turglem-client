51143304447070dd72f52620c702e01adef55a6d
Peter Leonov <gojpeg@gmail.com>
2010-02-09 10:09:36 +0300

    test 2 just prints a few forms now

--- test_02.cpp
+++ test_02.cpp
@@ -7,45 +7,49 @@ void do_cpp_magic(const tl::lemmatizer &lem, const char *s)
 {
 	tl::lem_result lr;
 	size_t sz_lem = lem.lemmatize<russian_utf8_adapter>(s, lr);
-
+	
 	if (sz_lem)
 	{
-		printf("Total lemmatize results: %u\n", sz_lem);
+		
 		for (size_t i = 0; i < sz_lem; i++)
 		{
-			printf("%u: paradigm/form = %d/%d\n", i,
-			    lem.get_paradigm(lr, i),
-			    lem.get_src_form(lr, i)
-			);
-			std::string nform = lem.get_text<russian_utf8_adapter>(lr, i, 0);
-			printf("\tnormal form: '%s'\n", nform.c_str());
+			printf("%d/%d:", lem.get_paradigm(lr, i), lem.get_src_form(lr, i));
+			printf(" %s", lem.get_text<russian_utf8_adapter>(lr, i, 0).c_str());
+			printf(" %s", lem.get_text<russian_utf8_adapter>(lr, i, 1).c_str());
+			printf(" %s", lem.get_text<russian_utf8_adapter>(lr, i, 2).c_str());
+			printf(" %s", lem.get_text<russian_utf8_adapter>(lr, i, 3).c_str());
+			printf(" %s", lem.get_text<russian_utf8_adapter>(lr, i, 4).c_str());
+			printf(" %s", lem.get_text<russian_utf8_adapter>(lr, i, 5).c_str());
+			printf("\n");
 		}
-	}
-	else
-	{
-		printf("Empty result! Empty string? If not, is it in in Russian?\n");
 	}
 }
 
 int main(int argc, char **argv)
 {
 	tl::lemmatizer lem;
-
+	
+	if (argc != 2)
+	{
+		printf("usage: %s водка\n", argv[0]);
+		return 1;
+	}
+	
 	try
 	{
 		lem.load_lemmatizer("/usr/local/share/turglem/russian/dict_russian.auto",
 		    "/usr/local/share/turglem/russian/paradigms_russian.bin",
 		    "/usr/local/share/turglem/russian/prediction_russian.auto"
 		);
-
-		do_cpp_magic(lem, "аццкая");
-		do_cpp_magic(lem, "находящиеся");
+		
+		do_cpp_magic(lem, argv[1]);
 	}
 	catch (const std::exception &e)
 	{
 		printf("EXCEPTION: %s\n", e.what());
+		return 1;
 	}
-
+	
 	return 0;
 }
 
